4d31d52ae0079a60b81839ee345d604f7b04ce75
gantar22 <nathancarterwilliams@gmail.com>
2020-02-10 22:59:47 -0500

    set up spells as a combination of combinator data and visual data

---
+++ Assets/Code/Game/Spell.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Implicitus/New Spell")]
+public class Spell : ScriptableObject
+{
+    [SerializeField]
+    Combinator combinator;
+
+    [SerializeField] private Sprite sprite;
+    
+}
