9da896f73e81e79481325f40666631b3254b0e6e
Zygnius <twjia1998@gmail.com>
2020-02-16 00:40:24 -0500

    Basic Main Menu Structure

---
+++ Assets/UI/Scripts/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+#pragma warning disable 0649
+    [SerializeField] private CanvasGroup mainMenu;
+    [SerializeField] private GameObject codex;
+    [SerializeField] private GameObject credits;
+
+#pragma warning restore 0649
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Quit();
+        }
+    }
+
+    public void Play()
+    {
+        //Load main gameplay scene
+    }
+
+    public void Codex()
+    {
+        //switch to codex
+    }
+
+    public void Credits()
+    {
+        //switch to credits
+    }
+
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else 
+        Application.Quit();
+#endif
+    }
+}
