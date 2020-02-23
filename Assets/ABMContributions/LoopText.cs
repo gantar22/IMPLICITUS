fd9e58adcddfe5bd6840dc7195fbb0f8602a0c57
abmeza <55360413+abmeza@users.noreply.github.com>
2020-02-23 17:43:00 -0500

    ABM Additions
    Added some fun text stuff that doesnt work well but currently exists, may scrap later

---
+++ Assets/ABMContributions/LoopText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopText : MonoBehaviour
+{
+    [SerializeField] UnitEvent goneOverBounds;
+    private RectTransform rect;    //Holds Transform of text object
+
+    //Function that offsets texts(hopefully puts it under canvas so it can scroll past again)
+    void RecreateText()
+    {
+        rect.transform.position -= transform.up * 250;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        goneOverBounds.AddListener(RecreateText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
