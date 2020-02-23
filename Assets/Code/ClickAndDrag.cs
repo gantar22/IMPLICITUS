3ee52d32ae969629cc912fe26d4423d7eeb6a73f
Katherine Wang <kathylu.wang@gmail.com>
2020-02-23 17:15:29 -0500

    click and drag level 2

---
+++ Assets/Code/ClickAndDrag.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAndDrag : MonoBehaviour
+{
+    [SerializeField] private SymbolManager SM;
+
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = false;
+
+    void Update()
+    {
+        if (isBeingHeld)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+        }
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(this.gameObject);
+            Vector3 mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isBeingHeld = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+
+        Drop(this.transform.localPosition);
+
+    }
+
+    private void Drop(Vector3 location) 
+    {
+        if () // If location is not valid, destroy this object
+        {
+            Destroy(this);
+        }
+        else // If location is valid, add to correct location by changing skeleton hierarchy and symbol hierarchy
+        {
+
+        }
+    }
+}
