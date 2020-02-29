a681a50df8e0e9bd0c8d102f80568a56ee12f685
Zygnius <twjia1998@gmail.com>
2020-02-29 14:52:07 -0500

    Level and Chapter data structures

---
+++ Assets/Data/Levels/Code/Level.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Implicitus/New Level")]
+public class Level : ScriptableObject
+{
+    [SerializeField] private int _num;
+    [SerializeField] private string _name;
+    [SerializeField] private string _description;
+    [SerializeField] private Spell[] _basis;
+    [SerializeField] private Spell _goal;
+
+    public int Num
+    {
+        get
+        {
+            return _num;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return _name;
+        }
+    }
+
+    public string Description
+    {
+        get
+        {
+            return _description;
+        }
+    }
+
+    public Spell[] Basis
+    {
+        get
+        {
+            return _basis;
+        }
+    }
+
+    public Spell Goal
+    {
+        get
+        {
+            return _goal;
+        }
+    }
+
+}
