29d5de52eb208bc7f326f91b193bf9cebf155760
abmeza <55360413+abmeza@users.noreply.github.com>
2020-03-22 16:35:17 -0400

    Level Complete Screen
    Pushing a level complete screen Prefab and completed level monobehavior that will be able to open window that allows user to continue to the next level

---
+++ Assets/ABMCurrentWork/CompletedLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompletedLevel : MonoBehaviour
+{
+    /* Variables */
+
+    [SerializeField] UnitEvent complete; //Event that will listen for level completed
+
+    [SerializeField] GameObject completedScreenPrefab; //Hold prefab of level complete screen
+    [SerializeField] Transform parentCanvasTransform; //Parent Canvas that will be used
+
+
+    /* Functions */
+    // Creates the Level Complete Screen
+    public void createLevelCompleteScreen()
+    {
+        GameObject completeScreen = (GameObject)Instantiate(completedScreenPrefab,
+                                                            parentCanvasTransform);
+    }
+
+    void Start()
+    {
+        //Initiate the Listener to create screen
+        complete.AddListener(createLevelCompleteScreen);
+    }
+
+}
