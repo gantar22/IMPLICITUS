bb626760b75ff5c5a54c6803324aacf7089fecb6
Dmcdominic <dmcdominic@gmail.com>
2020-05-24 15:31:53 -0400

    LevelRandom improved to now increase the number of variables until the term works (up to a limit), and has a max number of combinators. Also excluded Peng, and decreased evaluation limit to 15.

--- Assets/Code/Game/Level/LevelLoader.cs
+++ Assets/Code/Game/Level/LevelLoader.cs
@@ -204,105 +204,118 @@ public class LevelLoader : ScriptableObject {
 
 		// ---- Settings ----
 		int numCombinatorsToApply = Random.Range(2, 5); // How many combinators will be part of the solution (inclusive, exclusive).
-		int numVariables = numCombinatorsToApply + Random.Range(-1, 3); // How many variables will be used.
 		int numAddedToBasis = Random.Range(0, 2); // How many additional (random) spells will be added to the basis.
 		float doubleCombinatorOdds = 0.4f; // For each combinator, the odds of it getting applied twice somewhere in the solution.
-		int evaluationLimit = 20; // Limit on the number of times a term can be evaluated, to detect (likely) infinite loops.
+		const int EVALUATION_LIMIT = 15; // Limit on the number of times a term can be evaluated, to detect (likely) infinite loops.
+		const int MAX_VARIABLES = 5; // Limit on the the number of variables that can be in the term.
+		const int MAX_COMBINATORS = 4; // Limit on the number of combinators that can go into the basis at all (because more won't fit well into the left panel).
+
+		// Make sure not to add an excessive number of combinators to the basis
+		while (numAddedToBasis + numCombinatorsToApply > MAX_COMBINATORS) {
+			numAddedToBasis--;
+		}
 
 		// ---- Term Generation ----
 		// Choose the random spells and start constructing the Term leafList
 		List<Spell> spellsToApply = new List<Spell>();
-		List<Term> leafList = new List<Term>();
-		for (int i=0; i < numCombinatorsToApply; i++) {
+		List<Term> baseLeafList = new List<Term>();
+		for (int i = 0; i < numCombinatorsToApply; i++) {
 			Spell randSpell = allUsableSpells[Random.Range(0, usableSpellsCount)];
 			spellsToApply.Add(randSpell);
-			leafList.Add(Term.Leaf(Sum<Combinator, Lambda.Variable>.Inl(randSpell.combinator)));
+			baseLeafList.Add(Term.Leaf(Sum<Combinator, Lambda.Variable>.Inl(randSpell.combinator)));
 			// Some odds to add this combinator twice
 			if (Random.Range(0f, 1f) < doubleCombinatorOdds) {
-				leafList.Add(Term.Leaf(Sum<Combinator, Lambda.Variable>.Inl(randSpell.combinator)));
+				baseLeafList.Add(Term.Leaf(Sum<Combinator, Lambda.Variable>.Inl(randSpell.combinator)));
 			}
 		}
-		
+
 		// Shuffle
-		for (int i = 0; i < leafList.Count - 1; i++) {
-			int toSwap = Random.Range(i, leafList.Count);
-			Term tempLeaf = leafList[toSwap];
-			leafList[toSwap] = leafList[i];
-			leafList[i] = tempLeaf;
+		for (int i = 0; i < baseLeafList.Count - 1; i++) {
+			int toSwap = Random.Range(i, baseLeafList.Count);
+			Term tempLeaf = baseLeafList[toSwap];
+			baseLeafList[toSwap] = baseLeafList[i];
+			baseLeafList[i] = tempLeaf;
 		}
 
-		// Add the variables to the leafList
-		for (int i=0; i < numVariables; i++) {
-			leafList.Add(Term.Leaf(Sum<Combinator, Lambda.Variable>.Inr((Lambda.Variable)i)));
-		}
+		// Test increasingly large number of variables
+		for (int numVariables = 1; numVariables <= MAX_VARIABLES; numVariables++) {
+			List<Term> leafList = new List<Term>(baseLeafList);
+			// Add the variables to the leafList
+			for (int i = 0; i < numVariables; i++) {
+				leafList.Add(Term.Leaf(Sum<Combinator, Lambda.Variable>.Inr((Lambda.Variable)i)));
+			}
 
-		// Construct the Term node from the leafList
-		Term term = Term.Node(leafList);
+			// Construct the Term node from the leafList
+			Term term = Term.Node(leafList);
 
-		int evaluationCount = 0;
-		List<Lambda.ElimRule> elimRules = Lambda.Util.CanEvaluate(term, new List<int>(), (v, rule) => rule);
-		while (elimRules.Count > 0 && evaluationCount < evaluationLimit) {
-			evaluationCount++;
-			term = elimRules[0].evaluate(term);
-			elimRules = Lambda.Util.CanEvaluate(term, new List<int>(), (v, rule) => rule);
-		}
+			int evaluationCount = 0;
+			List<Lambda.ElimRule> elimRules = Lambda.Util.CanEvaluate(term, new List<int>(), (v, rule) => rule);
+			while (elimRules.Count > 0 && evaluationCount < EVALUATION_LIMIT) {
+				evaluationCount++;
+				term = elimRules[0].evaluate(term);
+				elimRules = Lambda.Util.CanEvaluate(term, new List<int>(), (v, rule) => rule);
+			}
 
-		// Have to check if it could successfully (and fully) evaluate. Otherwise retry
-		if (evaluationCount >= evaluationLimit || containsCombinator(term)) {
-			rerandomizeLevelRandom();
-			return;
-		}
+			// Have to check if it could successfully (and fully) evaluate. Otherwise retry
+			if (evaluationCount >= EVALUATION_LIMIT || containsCombinator(term)) {
+				// If it fails, try an additional variable (or reach the limit and try a completely new term)
+				continue;
+			}
+			// If it succeeds, complete the generation
 
-		// ---- Lambda Term ----
-		// Generate the first half of the lambdaTerm (string)
-		levelRandom.Goal.lambdaTerm = "R ";
-		for (int i=0; i < numVariables; i++) {
-			levelRandom.Goal.lambdaTerm += (char)(122 - i) + " ";
-		}
-		levelRandom.Goal.lambdaTerm += "=> ";
+			// ---- Lambda Term ----
+			// Generate the first half of the lambdaTerm (string)
+			levelRandom.Goal.lambdaTerm = "R ";
+			for (int i = 0; i < numVariables; i++) {
+				levelRandom.Goal.lambdaTerm += (char)(122 - i) + " ";
+			}
+			levelRandom.Goal.lambdaTerm += "=> ";
 
-		// Generate the second half of the lambdaTerm (See Shrub.toString() as reference)
-		string resultLambdaString = string.Join(" ", term.MapPreorder(
-			t => t.Match(combin => combin.ToString(), variab => variab.ToString()),
-			l => l.Prepend("(").Append(")").ToList()
-			));
+			// Generate the second half of the lambdaTerm (See Shrub.toString() as reference)
+			string resultLambdaString = string.Join(" ", term.MapPreorder(
+				t => t.Match(combin => combin.ToString(), variab => variab.ToString()),
+				l => l.Prepend("(").Append(")").ToList()
+				));
 
-		// Convert the variable ints to variables
-		for (int i = 0; i < numVariables; i++) {
-			resultLambdaString = resultLambdaString.Replace(i.ToString().ToCharArray()[0], (char)(122 - i));
-		}
+			// Convert the variable ints to variables
+			for (int i = 0; i < numVariables; i++) {
+				resultLambdaString = resultLambdaString.Replace(i.ToString().ToCharArray()[0], (char)(122 - i));
+			}
 
-		// Trim off the first and last parens
-		resultLambdaString = resultLambdaString.Remove(0, 1);
-		resultLambdaString = resultLambdaString.Remove(resultLambdaString.Length - 1, 1);
+			// Trim off the first and last parens
+			resultLambdaString = resultLambdaString.Remove(0, 1);
+			resultLambdaString = resultLambdaString.Remove(resultLambdaString.Length - 1, 1);
 
-		// Finally, append it to the lambdaTerm (string)
-		levelRandom.Goal.lambdaTerm += resultLambdaString;
+			// Finally, append it to the lambdaTerm (string)
+			levelRandom.Goal.lambdaTerm += resultLambdaString;
 
-		// ---- Basis ----
-		// Add numAddedToBasis random combinators to the list
-		for (int i=0; i < numAddedToBasis; i++) {
-			Spell randSpell = allUsableSpells[Random.Range(0, usableSpellsCount)];
-			spellsToApply.Insert(Random.Range(0, spellsToApply.Count), randSpell);
-		}
-		// Deduplicate
-		for (int i=0; i < spellsToApply.Count; i++) {
-			for (int j = i + 1; j < spellsToApply.Count; j++) {
-				if (spellsToApply[i] == spellsToApply[j]) {
-					spellsToApply.RemoveAt(j);
-					i = 0;
-					j = 0;
+			// ---- Basis ----
+			// Add numAddedToBasis random combinators to the list
+			for (int i = 0; i < numAddedToBasis; i++) {
+				Spell randSpell = allUsableSpells[Random.Range(0, usableSpellsCount)];
+				spellsToApply.Insert(Random.Range(0, spellsToApply.Count), randSpell);
+			}
+			// Deduplicate
+			for (int i = 0; i < spellsToApply.Count; i++) {
+				for (int j = i + 1; j < spellsToApply.Count; j++) {
+					if (spellsToApply[i] == spellsToApply[j]) {
+						spellsToApply.RemoveAt(j);
+						i = 0;
+						j = 0;
+					}
 				}
 			}
+			// Shuffle
+			for (int i = 0; i < spellsToApply.Count - 1; i++) {
+				int toSwap = Random.Range(i, spellsToApply.Count);
+				Spell tempSpell = spellsToApply[toSwap];
+				spellsToApply[toSwap] = spellsToApply[i];
+				spellsToApply[i] = tempSpell;
+			}
+			levelRandom.secretlySetBasis(spellsToApply.ToArray());
+			return;
 		}
-		// Shuffle
-		for (int i=0; i < spellsToApply.Count - 1; i++) {
-			int toSwap = Random.Range(i, spellsToApply.Count);
-			Spell tempSpell = spellsToApply[toSwap];
-			spellsToApply[toSwap] = spellsToApply[i];
-			spellsToApply[i] = tempSpell;
-		}
-		levelRandom.secretlySetBasis(spellsToApply.ToArray());
+		rerandomizeLevelRandom();
 	}
 
 
